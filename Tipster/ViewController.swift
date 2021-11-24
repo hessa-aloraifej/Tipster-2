86a1d2ebdb0760d2eac554945a1aa3beb992a9a5
Hessa Aloraifej <aloraifej.hessa@gmail.com>
2021-11-24 10:57:36 +0300

    Initial Commit

---
+++ Tipster/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tipster
+//
+//  Created by Hessa Aloraifej on 24/11/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
